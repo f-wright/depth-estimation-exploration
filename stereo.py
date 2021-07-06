50fe9fec84445eab9e06fbc936f9f9ce5a8fb583
f-wright <francinerwright@gmail.com>
2021-07-06 13:55:24 -0700

    reorganized and added texture threshold

--- stereo.py
+++ stereo.py
@@ -3,26 +3,32 @@ import cv2
 from matplotlib import pyplot as plt
 from matplotlib import cm
 
-fx = 7190.247       # lense focal length
-baseline = 174.945  # distance in mm between the two cameras (values from middlebury)
-disparities = 16    # num of disparities to consider
-block = 5           # block size to match
-units = 0.001       # depth units
-doffs=342.523       # x-difference of principal points, following https://vision.middlebury.edu/stereo/data/scenes2014/#description
+fx = 7190.247               # lense focal length
+baseline = 174.945          # distance in mm between the two cameras (values from middlebury)
+disparities = 16            # num of disparities to consider
+block = 15                  # block size to match
+units = 0.001               # depth units
+doffs=342.523               # x-difference of principal points, following https://vision.middlebury.edu/stereo/data/scenes2014/#description
+
+texture_threshold = 2000      # 10 by default
 
 
-imgL = cv2.imread('im0.png', 0)
-imgR = cv2.imread('im1.png', 0)
+imgL = cv2.imread('images/im0_left.png', 0)
+imgR = cv2.imread('images/im0_right.png', 0)
 
 
 sbm = cv2.StereoBM_create(numDisparities=disparities,blockSize=block)
+sbm.setTextureThreshold(texture_threshold)
+
 
 # calculate disparities
 disparity = sbm.compute(imgL, imgR)
 print(disparity)
-# # show disparity
-# plt.imshow(disparity, 'gray')
-# plt.show()
+# show disparity
+plt.imshow(disparity, 'gray')
+plt.show()
+
+
 
 
 depth = np.zeros(shape=imgL.shape).astype(float)
